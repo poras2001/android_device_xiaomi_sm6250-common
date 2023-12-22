df174cc7d1619ed512112137051255cc03e66fbc
poras2001 <91818021+poras2001@users.noreply.github.com>
2023-12-22 08:43:12 +0530

    Update Android.mk

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,7 +4,29 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Papers
+LOCAL_OVERRIDES_PACKAGES := \
+    Papers \
+    Auxio \
+    Chrome \
+    Chrome-Stub \
+    Drive \
+    Maps \
+    MatLog \
+    Photos \
+    PrebuiltGmail \
+    RecorderPrebuilt \
+    SafetyHubPrebuilt \
+    Videos \
+    YouTube \
+    PlayAutoInstalls \
+    PixelLiveWallpaper \
+    AndroidDevicePolicy \
+    PixelTroubleshooting \
+    GooglePartnerSetup \
+    PixelBuds \
+    DevicePolicy
+    Cross-Device Services
+
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
